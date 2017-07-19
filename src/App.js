2035e47b5a67d49f4451aed4395b33e9cfaa59f7
Mitchell Henson <mitchell.b.henson@gmail.com>
2017-07-19 09:04:40 +0200

    Updating local version of node and therefore updating npm-shrinkwrap lockversion. Adding scss and some images and starting on CSS changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import mitch from './mitch.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-Body">
+          <img className='img-circle' src={mitch}></img>
+          <h5>Another Quirky Dev</h5>
+          <p>Mitchell Henson</p>
+        </div>
       </div>
     );
   }
