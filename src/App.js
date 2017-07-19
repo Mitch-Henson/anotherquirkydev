5a36fd14e9663f8e1c15e0665045eea5ccfa7af1
Mitchell Henson <mitchell.b.henson@gmail.com>
2017-07-19 22:25:04 +0200

    Adding new potential header images, removing older, smaller images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import mitch from './mitch.jpg';
+import mitch from './images/mitch.jpg';
 import './App.css';
 
 class App extends Component {
