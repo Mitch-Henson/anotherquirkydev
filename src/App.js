65ca2af5f5a00a0a8d481db18b2787d358bf03ba
Mitchell Henson <mitchell.b.henson@gmail.com>
2017-07-30 19:06:42 +0200

    Updating css to center background image, adding github link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import mitch from './images/mitch.jpg';
+import github from './images/github.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-        </div>
-        <div className="App-Body">
-          <img className='img-circle' src={mitch}></img>
-          <h5>Another Quirky Dev</h5>
-          <p>Mitchell Henson</p>
+      <div className='App'>
+        <div className='App-header'/>
+        <div className='App-Body'>
+          <img className='img-circle' src={mitch} alt='Picture of Mitchell Henson'/>
+          <h3>Mitchell Henson</h3>
+          <p>Australian developer based in Berlin</p>
+          <a href='https://github.com/Mitch-Henson/'>
+            <img src={github} alt='Github'/>
+          </a>
         </div>
       </div>
     );
